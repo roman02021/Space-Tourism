a02066c105c2494c3bd718ee4fee39cdf29bc537
roman02021 <roman.zemanik@revolware.com>
2022-01-15 18:44:12 +0100

    Reworked the line on desktop and finished menu on all devices.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+window.onload = () => {
+
+//Selected menu item -- START
 let selected = 0;
 for (var i = 0; i < document.links.length; i++) {
     if (document.links[i].href === document.URL) {
@@ -5,3 +8,55 @@ for (var i = 0; i < document.links.length; i++) {
     }
 }
 document.links[selected].classList.add('selected');
+//Selected menu item -- END
+
+//Menu item container width -- START
+const root = document.documentElement;
+
+const menuItemsContainerWidth = document.querySelector('.js-menu-items-container').clientWidth;
+const listContainer = document.querySelector('.js-list-container');
+
+console.log(document.querySelector('.js-menu-items-container').clientWidth);
+root.style.setProperty('--menu-items-container', `${menuItemsContainerWidth}px`);
+// listContainer.classList.add('loaded');
+
+const mediaQuery = window.matchMedia('(max-width: 1158px)');
+
+//Recalculate menuItemsContainerWidth variable when coming from less than desktop resolution
+mediaQuery.addEventListener('change', e => {
+if (!e.matches) {
+    root.style.setProperty('--menu-items-container', `${menuItemsContainerWidth}px`);
+}
+})
+
+//Menu item container width -- END
+
+
+//Hamburger -- START
+const hamburgerBtn = document.querySelector('.js-hamburger');
+const hamburgerCloseBtn = document.querySelector('.js-hamburger-close');
+const menuContainer = document.querySelector('.js-menu-container');
+const page = document.querySelector('.js-page');
+
+hamburgerBtn.addEventListener('click', ()=>{
+    menuContainer.classList.add('open');
+    page.addEventListener('click', ()=>{
+        menuContainer.classList.remove('open');
+    })
+})
+hamburgerCloseBtn.addEventListener('click', ()=>{
+    menuContainer.classList.remove('open');
+    page.removeEventListener('click', ()=>{
+        menuContainer.classList.remove('open');
+    })
+})
+//Hamburger -- END
+
+}
+
+
+//I needed to set the variable before onload because the header menu was jumping on every page load
+//I need to set the same variable again in onload because sometimes the script runs before css and then menu itemsContainerWidth has wrong value
+const root = document.documentElement;
+const menuItemsContainerWidth = document.querySelector('.js-menu-items-container').clientWidth;
+root.style.setProperty('--menu-items-container', `${menuItemsContainerWidth}px`);
--- web-components/header.js
+++ web-components/header.js
@@ -7,14 +7,24 @@ class Header extends HTMLElement {
               <img class="menu__logo" src="../assets/shared/logo.svg">
             </a>
           </div>        
-          <div class="menu__line-through"></div>
-          <nav class="menu__container">
-            <ul class="menu__list">
+          <button class="hamburger js-hamburger">
+            <span class="hamburger__line"></span>
+            <span class="hamburger__line"></span>
+            <span class="hamburger__line"></span>
+          </button>
+          <nav class="menu__container js-menu-container">
+            <div class="menu__list-container js-list-container">
+            <ul class="menu__list js-menu-items-container">
+              <div class="close-container">
+                <button class="hamburger-close js-hamburger-close">
+                </button>
+              </div>
               <li class="menu__item"><a href="index.html" class="menu__link"><span class="menu__count">00</span><span class="menu__text">Home</span></a></li>
               <li class="menu__item"><a href="destination.html" class="menu__link"><span class="menu__count">01</span><span class="menu__text">Destination</span></a></li>
               <li class="menu__item"><a href="crew.html" class="menu__link"><span class="menu__count">02</span><span class="menu__text">Crew</span></a></li>
               <li class="menu__item"><a href="technology.html" class="menu__link"><span class="menu__count">03</span><span class="menu__text">Technology</span></a></li>
             </ul>
+            </div>
           </nav>
         </header>
       `;
